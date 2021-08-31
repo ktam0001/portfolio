7ad3513c15107a89a93c263f7480720022707584
KTam <karantamrakar8@gmail.com>
2021-09-01 00:02:44 +0530

    enhance_kt_2

--- src/components/CoreProjectGrid.js
+++ src/components/CoreProjectGrid.js
@@ -1,42 +1,24 @@
 import React from 'react'
+import projectsList from './ProjectsList';
 import './styles/coreprojectgrid.css';
-
 
 // core-div-2-img1
 // <img classname = 'core-div-2img' src = {ReactJSimg} alt = ''/>
 const CoreProjectGrid = (props) => {
+    const reuse = () => {
+        return projectsList.map(project => {
+            return (
+                <div className = {`core-div-2 ${project.coreDivImg}`}>
+                    <h3 className = 'core-h3'>{project.projectName}<br/><br/></h3>
+                    <p className = 'core-p-1'>Key Skills : {project.keySkills}</p>
+                    <p className = 'core-p-2'>Highlights : {project.highlights}</p>
+                </div>
+            );
+        });
+    }
     return (
         <div className = 'core-div-1'>
-            <div className = 'core-div-2 core-div-2-img1'>
-                <h3 className = 'core-h3'>Dashboard Implementation<br/><br/></h3>
-                <p className = 'core-p-1'>Key Skills : ReactJS, JavaScript</p>
-                <p className = 'core-p-2'>Highlights : Development and maintenance</p>
-            </div>
-            <div className = 'core-div-2 core-div-2-img2'>
-                <h3 className = 'core-h3'>Portfolio Webapp<br/><br/></h3>
-                <p className = 'core-p-1'>Key Skills : ReactJS, JS</p>
-                <p className = 'core-p-2'>Highlights : Development and maintenance</p>
-            </div>
-            <div className = 'core-div-2 core-div-2-img3'>
-                <h3 className = 'core-h3'>API Integration<br/><br/></h3>
-                <p className = 'core-p-1'>Key Skills : Python, JS, ReactJS, Django, DRF</p>
-                <p className = 'core-p-2'>Highlights : Backend development, UI development, Integrated the API with the UI, REST API, JWT</p>
-            </div>
-            <div className = 'core-div-2 core-div-2-img4'>
-                <h3 className = 'core-h3'>DataPower<br/><br/></h3>
-                <p className = 'core-p-1'>Key Skills : Python, DRF</p>
-                <p className = 'core-p-2'>Highlights : Developement and maintenance of REST services</p>
-            </div>
-            <div className = 'core-div-2 core-div-2-img5'>
-                <h3 className = 'core-h3'>ADIA<br/><br/></h3>
-                <p className = 'core-p-1'>Key Skills : Python</p>
-                <p className = 'core-p-2'>Highlights : Analysis of requirements, design, develop and integrating the algorithm</p>
-            </div>
-            <div className = 'core-div-2 core-div-2-img6'>
-                <h3 className = 'core-h3'>NetVault<br/><br/></h3>
-                <p className = 'core-p-1'>Key Skills : Functional Testing</p>
-                <p className = 'core-p-2'>Highlights : Writing test-cases, testing and delivering the product to the consumer</p>
-            </div>
+            {reuse()}    
         </div>
     )
 }
---
+++ src/components/ProjectsList.js
@@ -0,0 +1,40 @@
+const projectsList = [   
+    {
+        coreDivImg : "core-div-2-img1", 
+        projectName : "Dashboard Implementation", 
+        keySkills : "ReactJS, JavaScript", 
+        highlights : "Development and maintenance"
+    },
+    {
+        coreDivImg : "core-div-2-img2", 
+        projectName : "Portfolio Webapp", 
+        keySkills : "ReactJS, JavaScript", 
+        highlights : "Development and maintenance"
+    },
+    {
+        coreDivImg : "core-div-2-img3", 
+        projectName : "API Integration", 
+        keySkills : "Python, JS, ReactJS, Django, DRF", 
+        highlights : "Backend development, UI development, Integrated the API with the UI, REST API, JWT"
+    },
+    {
+        coreDivImg : "core-div-2-img4", 
+        projectName : "DataPower", 
+        keySkills : "Python, DRF", 
+        highlights : "Developement and maintenance of REST services"
+    },
+    {
+        coreDivImg : "core-div-2-img5", 
+        projectName : "ADIA", 
+        keySkills : "Python", 
+        highlights : "Analysis of requirements, design, develop and integrating the algorithm"
+    },
+    {
+        coreDivImg : "core-div-2-img6", 
+        projectName : "NetVault", 
+        keySkills : "Functional Testing", 
+        highlights : "Writing test-cases, testing and delivering the product to the consumer"
+    }
+]
+
+export default projectsList;
