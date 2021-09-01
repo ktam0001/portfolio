2dc00322e134ce7517773410ba4275af4d4f8721
shubham asthana <shuastha@cisco.com>
2021-09-01 17:22:11 +0530

    removed static code and moved contents to constants

--- src/components/GenGrid.js
+++ src/components/GenGrid.js
@@ -1,39 +1,21 @@
 import React from 'react'
-import js_icon from './icons/js_icon.png';
-import reactjs_icon from './icons/reactjs_icon.png';
-import python_icon from './icons/python_icon.png';
-import django_icon3 from './icons/django_icon3.png';
-import restapi_icon from './icons/restapi_icon.png';
-import redux_icon from './icons/redux_icon.png';
+import {skills} from '../constants/grid-constants'
 import './styles/footer.css';
 
 const GenGrid = (props) => {
+    const renderSkills = ()=> {
+       return skills.map((skill, i) => {
+            return (  
+            <div key={i}className = {props.css.footer_nested_div}>
+            <img src = {skill.src} alt = {skill.alt}/>
+            <label className= {props.css.footer_img_label}>{skill.skill}</label>
+        </div>
+            )
+        })
+    }
     return (
         <div className = {props.css.footer_main_div}>
-            <div className = {props.css.footer_nested_div}>
-                <img src = {js_icon} alt = "js_icon"/>
-                <label className= {props.css.footer_img_label}>JavaScript</label>
-            </div>
-            <div className = {props.css.footer_nested_div}>
-                <img src = {reactjs_icon} alt = "reactjs_icon"  />
-                <label className= {props.css.footer_img_label}>ReactJS</label>
-            </div>
-            <div className = {props.css.footer_nested_div}>
-                <img src = {redux_icon} alt = "redux_icon"  />
-                <label className= {props.css.footer_img_label}>Redux</label>
-            </div>
-            <div className = {props.css.footer_nested_div}>
-                <img src = {python_icon} alt = "python_icon"  />
-                <label className= {props.css.footer_img_label}>Python</label>
-            </div>
-            <div className = {props.css.footer_nested_div}>
-                <img src = {django_icon3} alt = "django_icon3"  />
-                <label className= {props.css.footer_img_label}>Django</label>
-            </div>
-            <div className = {props.css.footer_nested_div}>
-                <img src = {restapi_icon} alt = "restapi_icon"  />
-                <label className= {props.css.footer_img_label}>DRF</label>
-            </div>
+            {renderSkills()}
         </div>
     )
 }
